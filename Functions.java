c7512ffa0f9382d654f478ca6e19b4b6e5bd71cf
ushae9 <ushae955@gmail.com>
2026-01-20 08:34:07 +0200

    Add files via upload

---
+++ Functions.java
@@ -0,0 +1,95 @@
+                                            // JAVA FUNCTIONS
+       /* Functions is a block of code to perform specific task.
+        mainly for good struture & reuse
+        it will run when we call
+         
+       detatype function name () {
+                   // function body
+
+             }
+
+     ex: int addition(int ope1,ope2)//function definition
+          int is datatype
+          addition is method name
+          int ope1,int ope2 is parameters
+          {
+            int sum;
+            sum= ope1+ope2;//function body
+            return sum;
+            }
+              addition (20,30)//function call */ 
+
+       /* public class Functions
+        {
+             public static void sum(int a, int b)
+              {
+                int c;
+                 c=a+b;
+                 System.out.println("Sum of " + a + " and " + b + " is " + c);
+                 }
+ 		  public static void main(String args[])
+                   {
+                     sum(20,30);
+                    }
+                      }*/
+
+     /* public class Functions
+      {
+       public static void sum(int a, int b)
+       {
+        int c;
+        c=a+b;
+        System.out.println("Sum of " + a + " and " + b + " is " + c);
+        }
+        public static void main(String args[])
+        {
+          sum(20,30);
+         }
+           }*/
+
+       public class Functions
+       {
+        public static void sum(int a, int b)
+        {
+          int c;
+          c=a+b;
+          System.out.println("Sum of " + a + " and " + b + " is " + c);
+          }
+          public static void main(String args[])
+          {
+            sum(40,50);
+          }
+            }
+          
+          
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    
+
+       
